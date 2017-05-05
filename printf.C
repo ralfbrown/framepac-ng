92f0ad2b14f4ee7e21e2d071adc33467982bfd3e
ralfbrown <ralfbrown@users.noreply.github.com>
2017-05-05 07:52:36 -0400

    aprintf and vaprintf implementations

---
+++ printf.C
@@ -0,0 +1,67 @@
+/****************************** -*- C++ -*- *****************************/
+/*									*/
+/* FramepaC-ng								*/
+/* Version 0.01, last edit 2017-05-05					*/
+/*	by Ralf Brown <ralf@cs.cmu.edu>					*/
+/*									*/
+/* (c) Copyright 2016,2017 Carnegie Mellon University			*/
+/*	This program may be redistributed and/or modified under the	*/
+/*	terms of the GNU General Public License, version 3, or an	*/
+/*	alternative license agreement as detailed in the accompanying	*/
+/*	file LICENSE.  You should also have received a copy of the	*/
+/*	GPL (file COPYING) along with this program.  If not, see	*/
+/*	http://www.gnu.org/licenses/					*/
+/*									*/
+/*	This program is distributed in the hope that it will be		*/
+/*	useful, but WITHOUT ANY WARRANTY; without even the implied	*/
+/*	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR		*/
+/*	PURPOSE.  See the GNU General Public License for more details.	*/
+/*									*/
+/************************************************************************/
+
+#include <cstdarg>
+#include <cstdio>
+#include "framepac/texttransforms.h"
+
+namespace Fr
+{
+
+/************************************************************************/
+/************************************************************************/
+
+char* vaprintf(const char *fmt, va_list args)
+{
+   // we need to make a copy of the arglist, because va_lists are passed by
+   //   reference, so the first call to vsprintf would clobber 'args' for
+   //   the second call!
+   va_list argcopy ;
+   va_copy(argcopy,args) ;
+   size_t len = vsnprintf(nullptr,0,fmt,argcopy) ;
+   va_end(argcopy) ;
+   char* buf = new char[len+1] ;
+   if (buf)
+      {
+      va_copy(argcopy,args) ;
+      vsnprintf(buf,len+1,fmt,args) ;
+      va_end(argcopy) ;
+      buf[len+1] = '\0' ;
+      }
+   return buf ;
+}
+
+//----------------------------------------------------------------------------
+
+char* aprintf(const char *fmt, ...)
+{
+   va_list args ;
+   va_start(args,fmt) ;
+   char* s = vaprintf(fmt,args) ;
+   va_end(args) ;
+   return s ;
+}
+
+//----------------------------------------------------------------------------
+
+} // end namespace Fr
+
+// end of file printf.C //
