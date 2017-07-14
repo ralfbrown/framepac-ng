a1c064d6b848538f66775ec5d2ce2479ee774063
ralfbrown <ralfbrown@users.noreply.github.com>
2017-07-14 15:36:36 -0400

    first rudimentary version of tests/objtest

---
+++ tests/objtest.C
@@ -0,0 +1,44 @@
+/****************************** -*- C++ -*- *****************************/
+/*									*/
+/* FramepaC-ng								*/
+/* Version 0.01, last edit 2017-07-14					*/
+/*	by Ralf Brown <ralf@cs.cmu.edu>					*/
+/*									*/
+/* (c) Copyright 2017 Carnegie Mellon University			*/
+/*	This program may be redistributed and/or modified under the	*/
+/*	terms of the GNU General Public License, version 3, or an	*/
+/*	alternative license agreement as detailed in the accompanying	*/
+/*	file LICENSE.  You should also have received a copy of the	*/
+/*	GPL (file COPYING) along with this program.  If not, see	*/
+/*	http://www.gnu.org/licenses/					*/
+/*									*/
+/*	This program is distributed in the hope that it will be		*/
+/*	useful, but WITHOUT ANY WARRANTY; without even the implied	*/
+/*	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR		*/
+/*	PURPOSE.  See the GNU General Public License for more details.	*/
+/*									*/
+/************************************************************************/
+
+#include "framepac/objreader.h"
+#include "framepac/symboltable.h"
+
+using namespace Fr ;
+
+/************************************************************************/
+/************************************************************************/
+
+int main(int /*argc*/, char** /*argv*/)
+{
+   Fr::Initialize() ;
+   Symbol* NIL { SymbolTable::current()->add("NIL") } ;
+   Object* obj { nullptr } ;
+   while (obj != NIL)
+      {
+      cout << "Type an object, or NIL to quit: " << flush ;
+      cin >> obj ;
+      //FIXME
+      }
+   return 0 ;
+}
+
+// end of file objtest.C //
