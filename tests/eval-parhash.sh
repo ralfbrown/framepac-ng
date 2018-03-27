911ad9f7ff9a5080c76f7ffe107efdbbc780cf26
ralfbrown <ralfbrown@users.noreply.github.com>
2018-03-26 22:06:57 -0400

    enhanced eval-parhash.sh to take test size selection from commandline

--- tests/eval-parhash.sh
+++ tests/eval-parhash.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+sz=small
+if [ "x$1" == x-med ]; then
+    sz=med
+elif [ "x$1" == x-big ]; then
+    sz=big
+elif [ "x$1" == x-huge ]; then
+    sz=huge
+fi
+
 # the program to be run
 hashtest=bin/parhash
 
@@ -13,7 +22,7 @@ tables="-i -H -I"
 
 # the number of threads to use
 threads="1 2 3 4 6 9 12 24 48 96 192 384"	# hex-core w/ HT
-#threads="1 2 4 8 14 28 42 56 112 224 448"	# dual 14-core w/ HT
+#threads="1 2 4 8 14 20 28 42 56 112 224 448"	# dual 14-core w/ HT
 
 # the concurrency for Hopscotch
 concurrency="16 32 64 128 256"
@@ -28,36 +37,56 @@ seconds=5
 timelimit=600
 
 # the generation of key values
-keys="-k 1"	# default (pseudo-random sequence)
 #keys="-k 0"	# sequential (maximizes cache hits and possible fill factor)
-#keys="-k 4"	# apply Fasthash64 to sequential integers
+keys="-k 1"	# default (pseudo-random sequence)
+#keys="-k 2"	# fixed pseudo-random sequence
+#keys="-k 4"	# apply Fasthash64 to sequential integers (may cause inadvertent duplication)
 
-# the hash array size
-size=67100000  # ~2^26
-size=16720000  # ~2^24
+# the hash array size and number of items to put in the hash table, as a function of the requested test size
+# number of items is selected to yield a fill factor just under 75% after the initial fill
+if [ "$sz" == "huge" ]; then
+  size=1073600000 # ~2^30
+  count=788000000
+  let "seconds=2*seconds"
+elif [ "$sz" == "big" ]; then
+  size=268400000 # ~2^28
+  count=197000000
+  let "seconds=2*seconds"
+elif [ "$sz" == "med" ]; then
+  size=67100000  # ~2^26
+  count=49500000
+else # sze == small
+  size=16720000  # ~2^24
+  count=12400000
+fi
 
-# the number of items to put in the hash table
-count=12400000
+invoke_hash()
+{
+    algo=$1
+    thr=$2
+    concur=$3
+    timeout -k5 $timelimit $hashtest -T100 --time $seconds -j-$thr -s$size -g$count $concur $keys $algo
+    return
+}
 
 run_hash()
 {
     algo=$1
-    concur=$2
-    [ -n "$concur" ] && concur=-C$concur
-    for thr in $threads; do
-	timeout -k5 $timelimit $hashtest -T100 --time $seconds -j-$thr -s$size -g$count $concur $keys $algo
-    done
+    thr=$2
+    if [ "x$algo" = "x-H" ]; then
+	for concur in $concurrency; do
+	    invoke_hash $algo $thr -C$concur
+	done
+    else
+	invoke_hash $algo $thr ""
+    fi
     return
 }
 
 for rep in $(seq $reps); do
-    for algo in $tables; do
-	if [ "x$algo" = "x-H" ]; then
-	    for concur in $concurrency; do
-		run_hash $algo $concur
-	    done
-	else
-	    run_hash $algo
-	fi
+    for thr in $threads; do
+	for algo in $tables; do
+	    run_hash $algo $thr
+	done
     done
 done
