cfb53c328a20978a3093fb2fa938be4d340fc6c9
ralfbrown <ralfbrown@users.noreply.github.com>
2018-08-16 14:34:23 -0400

    some fleshing-out of Matrix and its subclasses

--- framepac/matrix.h
+++ framepac/matrix.h
@@ -1,7 +1,7 @@
 /****************************** -*- C++ -*- *****************************/
 /*									*/
 /* FramepaC-ng								*/
-/* Version 0.07, last edit 2018-07-16					*/
+/* Version 0.09, last edit 2018-08-16					*/
 /*	by Ralf Brown <ralf@cs.cmu.edu>					*/
 /*									*/
 /* (c) Copyright 2016,2017,2018 Carnegie Mellon University		*/
@@ -39,16 +39,59 @@ class Matrix : public Object
    public:
       // object factories
       static Matrix* create(size_t rows, size_t cols) ;
-      
-   protected:
-      Matrix(size_t rows, size_t cols) : m_rows(rows), m_cols(cols) {}
-      Matrix(const Matrix &) = default ;
-      ~Matrix() = default ;
+      virtual T& operator() (size_t row, size_t col) ;
+      virtual const T& operator() (size_t row, size_t col) const ;
 
    protected:
+      // *** creation/destruction ***
+      void* operator new(size_t) { return s_allocator.allocate() ; }
+      void operator delete(void* blk,size_t) { s_allocator.release(blk) ; }
+      Matrix(size_t rows, size_t cols) : m_rows(rows), m_cols(cols) {}
+      Matrix(const Matrix&) = default ;
+      virtual ~Matrix() = default ;
+
+   protected: // implementation functions for virtual methods
+      friend class FramepaC::Object_VMT<Matrix> ;
+
+      // *** type determination predicates ***
+      static bool isMatrix_(const Object*) { return true ; }
+      static const char* typeName_(const Object*) { return "Matrix" ; }
+
+      // *** copying ***
+
+      // *** destroying ***
+      static void free_(Object* obj) { delete static_cast<Matrix*>(obj) ; }
+      // use shallowFree() on a shallowCopy()
+      static void shallowFree_(Object* obj) { delete static_cast<Matrix*>(obj) ; }
+
+      // *** I/O ***
+      // generate printed representation into a buffer
+
+      // *** standard info functions ***
+      static size_t size_(const Object* obj)
+	 {
+	    const Matrix* m = static_cast<const Matrix*>(obj) ;
+	    return m->m_rows * m->m_cols ;
+	 }
+      static size_t empty_(const Object* obj) { return Matrix::size_(obj) == 0 ; }
+
+      // *** standard access functions ***
+      
+      // *** comparison functions ***
+
+      // *** iterator support ***
+
+      // *** STL compatibiity ***
+
+   protected: // data members
       size_t m_rows ;
       size_t m_cols ;
+   private: // static data
+      static Allocator s_allocator ;
    } ;
+
+template <typename T>
+Allocator Matrix<T>::s_allocator(FramepaC::Object_VMT<Matrix<T>>::instance(),sizeof(Matrix<T>)) ;
 
 /************************************************************************/
 /************************************************************************/
@@ -62,12 +105,37 @@ class FullMatrix : public Matrix<T>
    public:
       // object factories
       static FullMatrix* create(size_t rows, size_t cols) ;
-      
-   protected:
-      FullMatrix(size_t rows, size_t cols) : super(rows,cols) {} //FIXME
-      ~FullMatrix() ;
+      virtual T& operator() (size_t row, size_t col) { return m_matrix[row*this->m_cols + col] ; }
+      virtual const T& operator() (size_t row, size_t col) const { return m_matrix[row*this->m_cols + col] ; }
 
+   protected: // creation/destruction
+      void* operator new(size_t) { return s_allocator.allocate() ; }
+      void operator delete(void* blk,size_t) { s_allocator.release(blk) ; }
+      FullMatrix(size_t rows, size_t cols) : super(rows,cols) {} //FIXME
+      FullMatrix(const FullMatrix&) ;
+      virtual ~FullMatrix() ;
+
+   protected: // implementation functions for virtual methods
+      friend class FramepaC::Object_VMT<FullMatrix> ;
+
+      // *** type determination predicates ***
+      static const char* typeName_(const Object*) { return "FullMatrix" ; }
+
+      // *** copying ***
+
+      // *** destroying ***
+      static void free_(Object* obj) { delete static_cast<FullMatrix*>(obj) ; }
+      // use shallowFree() on a shallowCopy()
+      static void shallowFree_(Object* obj) { delete static_cast<FullMatrix*>(obj) ; }
+
+   protected: // data members
+      T* m_matrix ;
+   private: // static data
+      static Allocator s_allocator ;
    } ;
+
+template <typename T>
+Allocator FullMatrix<T>::s_allocator(FramepaC::Object_VMT<FullMatrix<T>>::instance(),sizeof(FullMatrix<T>)) ;
 
 /************************************************************************/
 /************************************************************************/
@@ -83,9 +151,14 @@ class SparseMatrix : public Matrix<T>
       static SparseMatrix* create(size_t rows, size_t cols) ;
       
    protected:
+      void* operator new(size_t) { return s_allocator.allocate() ; }
+      void operator delete(void* blk,size_t) { s_allocator.release(blk) ; }
       SparseMatrix(size_t rows, size_t cols) : super(rows,cols) {} //FIXME
-      ~SparseMatrix() ;
+      SparseMatrix(const SparseMatrix&) ;
+      virtual ~SparseMatrix() ;
 
+   private: // static data
+      static Allocator s_allocator ;
    } ;
 
 /************************************************************************/
@@ -100,13 +173,28 @@ class UpperTriangMatrix : public Matrix<T>
    public:
       // object factories
       static UpperTriangMatrix* create(size_t rows, size_t cols) ;
-      
+
+      virtual T& operator() (size_t row, size_t col)
+	 { if (col < row) return m_dummy ;
+           return m_matrix_rows[row][col-row] ; }
+      virtual const T& operator() (size_t row, size_t col) const
+	 { if (col < row) return m_dummy ;
+           return m_matrix_rows[row][col-row] ; }
+
    protected:
 
-   public:
+   protected:
+      void* operator new(size_t) { return s_allocator.allocate() ; }
+      void operator delete(void* blk,size_t) { s_allocator.release(blk) ; }
       UpperTriangMatrix(size_t rows, size_t cols) : super(rows,cols) {} //FIXME
-      ~UpperTriangMatrix() ;
+      UpperTriangMatrix(const UpperTriangMatrix&) ;
+      virtual ~UpperTriangMatrix() ;
 
+   protected: // data members
+      T  m_dummy ;
+      T** m_matrix_rows ;
+   private: // static data
+      static Allocator s_allocator ;
    } ;
 
 /************************************************************************/
@@ -125,9 +213,16 @@ class LowerTriangMatrix : public Matrix<T>
       static LowerTriangMatrix* create(size_t rows, size_t cols) ;
       
    protected:
+      void* operator new(size_t) { return s_allocator.allocate() ; }
+      void operator delete(void* blk,size_t) { s_allocator.release(blk) ; }
       LowerTriangMatrix(size_t rows, size_t cols) : super(rows,cols) {} //FIXME
+      LowerTriangMatrix(const LowerTriangMatrix&) ;
       ~LowerTriangMatrix() ;
 
+   protected: // data members
+      T** m_matrix_cols ;
+   private: // static data
+      static Allocator s_allocator ;
    } ;
 
 /************************************************************************/
@@ -143,10 +238,22 @@ class SymmetricMatrix : public UpperTriangMatrix<T>
       // object factories
       static SymmetricMatrix* create(size_t rows, size_t cols) ;
       
-   protected:
-      SymmetricMatrix(size_t rows, size_t cols) : super(rows,cols) {} //FIXME
-      ~SymmetricMatrix() ;
+      virtual T& operator() (size_t row, size_t col)
+	 { if (col < row) std::swap(row,col) ;
+	   return this->m_matrix_rows[row][col-row] ; }
+      virtual const T& operator() (size_t row, size_t col) const
+	 { if (col < row) std::swap(row,col) ;
+	   return this->m_matrix_rows[row][col-row] ; }
 
+   protected:
+      void* operator new(size_t) { return s_allocator.allocate() ; }
+      void operator delete(void* blk,size_t) { s_allocator.release(blk) ; }
+      SymmetricMatrix(size_t rows, size_t cols) : super(rows,cols) {} //FIXME
+      SymmetricMatrix(const SymmetricMatrix&) ;
+      virtual ~SymmetricMatrix() = default ;
+
+   private: // static data
+      static Allocator s_allocator ;
    } ;
 
 /************************************************************************/
@@ -163,12 +270,23 @@ class DiagonalMatrix : public Matrix<T>
       static DiagonalMatrix* create(size_t rows, size_t cols) ;
       
    protected:
+      void* operator new(size_t) { return s_allocator.allocate() ; }
+      void operator delete(void* blk,size_t) { s_allocator.release(blk) ; }
       DiagonalMatrix(size_t rows, size_t cols, size_t width) : super(rows,cols) {} //FIXME
-      ~DiagonalMatrix() ;
+      DiagonalMatrix(const DiagonalMatrix&) ;
+      virtual ~DiagonalMatrix() ;
+
+      virtual T& operator() (size_t row, size_t col)
+	 { if (col < row) std::swap(row,col) ;
+	   return this->m_matrix_rows[row][col-row] ; }
 
    protected:
       size_t m_width ;   // width of the band around the diagonal which is non-zero
 			 // m_width=0 is just the diagonal, m_width=1 is diag plus one on each side, etc.
+      T* m_matrix_elts ;
+      T  m_dummy ;
+   private: // static data
+      static Allocator s_allocator ;
    } ;
 
 //----------------------------------------------------------------------------
