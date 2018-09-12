0e33d355253d6ba2b944ce7499bf37ddbb23bb44
ralfbrown <ralfbrown@users.noreply.github.com>
2018-09-12 03:48:55 -0400

    make count of changes atomic so eliminate a TSan error

--- template/cluster.cc
+++ template/cluster.cc
@@ -665,7 +665,8 @@ bool assign_vector_to_nearest_center(size_t index, va_list args)
    auto centers = va_arg(args,const Array*) ;
    auto measure = va_arg(args,VM*) ;
    auto threshold = va_arg(args,double) ;
-   auto changes = va_arg(args,volatile size_t*) ;
+//   auto changes = va_arg(args,volatile size_t*) ;
+   auto changes = va_arg(args,Atomic<size_t>*) ;
    auto prog = va_arg(args,ProgressIndicator*) ;
    if (!vector) return false ;
    auto best_center = ClusteringAlgo<IdxT,ValT>::nearestNeighbor(vector,centers,measure,threshold) ;
@@ -692,7 +693,7 @@ size_t ClusteringAlgo<IdxT,ValT>::assignToNearest(const Array* vectors, const Ar
 {
    ThreadPool *tp = ThreadPool::defaultPool() ;
    if (!tp) return false ;
-   volatile size_t changes { 0 } ;
+   Atomic<size_t> changes { 0 } ;
    if (tp->parallelize(assign_vector_to_nearest_center<IdxT,ValT>,vectors->size(),vectors,
 	 centers,m_measure,threshold,&changes,prog))
       return changes ;
