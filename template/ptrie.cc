daa5242cfc6f7aa3f4d75f094cff031769665432
ralfbrown <ralfbrown@users.noreply.github.com>
2019-02-14 18:41:32 -0500

    merge contents of packedtrie.cc into ptrie.cc

--- template/ptrie.cc
+++ template/ptrie.cc
@@ -1,10 +1,10 @@
 /****************************** -*- C++ -*- *****************************/
 /*									*/
 /* FramepaC-ng								*/
-/* Version 0.01, last edit 2017-05-08					*/
+/* Version 0.14, last edit 2019-02-14					*/
 /*	by Ralf Brown <ralf@cs.cmu.edu>					*/
 /*									*/
-/* (c) Copyright 2017 Carnegie Mellon University			*/
+/* (c) Copyright 2019 Carnegie Mellon University			*/
 /*	This program may be redistributed and/or modified under the	*/
 /*	terms of the GNU General Public License, version 3, or an	*/
 /*	alternative license agreement as detailed in the accompanying	*/
@@ -25,13 +25,50 @@ namespace Fr
 {
 
 /************************************************************************/
-/*	Methods for template class PackedTrie				*/
 /************************************************************************/
 
 
+/************************************************************************/
+/*	Methods for template class PackedTrie				*/
+/************************************************************************/
 
+template <typename T, typename IdxT, unsigned bits>
+PackedTrie<T,IdxT,bits>::PackedTrie()
+{
 
+   return ;
+}
+
+//----------------------------------------------------------------------
+
+template <typename T, typename IdxT, unsigned bits>
+PackedTrie<T,IdxT,bits>::PackedTrie(const Trie<T,IdxT,bits>* /*in_trie*/)
+{
+
+   return ;
+}
+
+//----------------------------------------------------------------------
+
+template <typename T, typename IdxT, unsigned bits>
+PackedTrie<T,IdxT,bits>::PackedTrie(const char* /*filename*/)
+{
+
+   return ;
+}
+
+//----------------------------------------------------------------------
+
+template <typename T, typename IdxT, unsigned bits>
+void PackedTrie<T,IdxT,bits>::init(IdxT /*cap*/)
+{
+
+   return ;
+}
+
+//----------------------------------------------------------------------
 
 } // end namespace Fr
+
 
 // end of file ptrie.cc //
