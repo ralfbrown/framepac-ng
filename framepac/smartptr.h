5dba7d83b8f909fe06b2eff9404d80d12d06f66f
ralfbrown <ralfbrown@users.noreply.github.com>
2019-07-12 18:31:54 -0400

    rename member var from m_string to m_items to reflect that
    the class is used for more than character strings

--- framepac/smartptr.h
+++ framepac/smartptr.h
@@ -1,10 +1,10 @@
 /****************************** -*- C++ -*- *****************************/
 /*									*/
 /* FramepaC-ng								*/
-/* Version 0.09, last edit 2018-08-25					*/
+/* Version 0.14, last edit 2019-07-12					*/
 /*	by Ralf Brown <ralf@cs.cmu.edu>					*/
 /*									*/
-/* (c) Copyright 2018 Carnegie Mellon University			*/
+/* (c) Copyright 2018,2019 Carnegie Mellon University			*/
 /*	This program may be redistributed and/or modified under the	*/
 /*	terms of the GNU General Public License, version 3, or an	*/
 /*	alternative license agreement as detailed in the accompanying	*/
@@ -34,38 +34,38 @@ class NewPtr
       typedef T* pointer ;
       typedef T element_type ;
    public:
-      NewPtr() { m_string = nullptr ; }
-      NewPtr(unsigned N) { m_string = new T[N] ; }
+      NewPtr() { m_items = nullptr ; }
+      NewPtr(unsigned N) { m_items = new T[N] ; }
       NewPtr(unsigned N, const T* s, unsigned copyN)
-	 { m_string = new T[N] ; std::copy(s,s+copyN,m_string) ; }
-      NewPtr(T* s) { m_string = s ; }
+	 { m_items = new T[N] ; std::copy(s,s+copyN,m_items) ; }
+      NewPtr(T* s) { m_items = s ; }
       NewPtr(const NewPtr&) = delete ;
-      NewPtr(NewPtr& orig) { m_string = orig.move() ; }
-      NewPtr(NewPtr&& orig) { m_string = orig.move() ; }
+      NewPtr(NewPtr& orig) { m_items = orig.move() ; }
+      NewPtr(NewPtr&& orig) { m_items = orig.move() ; }
       ~NewPtr() { reset(nullptr) ; }
       NewPtr& operator= (const NewPtr&) = delete ;
       NewPtr& operator= (NewPtr& orig) { reset(orig.release()) ; return *this ; }
       NewPtr& operator= (NewPtr&& orig) { reset(orig.release()) ; return *this ; }
       NewPtr& operator= (T* new_s) { reset(new_s) ; return *this ; }
 
-      T* get() const noexcept { return m_string ; }
-      T* move() { T* s = m_string ; clear() ; return s ; }
-      void clear() { m_string = nullptr ; }
-      void reset(T* ptr) { T* old = m_string ; m_string = ptr ; delete[] old ; }
-      T* release() { T* ptr = m_string ; clear() ; return ptr ; }
+      T* get() const noexcept { return m_items ; }
+      T* move() { T* s = m_items ; clear() ; return s ; }
+      void clear() { m_items = nullptr ; }
+      void reset(T* ptr) { T* old = m_items ; m_items = ptr ; delete[] old ; }
+      T* release() { T* ptr = m_items ; clear() ; return ptr ; }
 
-      T* operator-> () { return m_string ; }
-      const T* operator-> () const { return m_string ; }
-      T* operator* () { return m_string ; }
-      const T* operator* () const { return m_string ; }
-      explicit operator T* () const { return m_string ; }
-      operator const T* () const { return m_string ; }
-      T& operator[] (size_t N) { return m_string[N] ; }
-      const T& operator[] (size_t N) const { return m_string[N] ; }
-      explicit operator bool () const { return m_string != nullptr ; }
-      bool operator ! () const { return m_string == nullptr ; }
+      T* operator-> () { return m_items ; }
+      const T* operator-> () const { return m_items ; }
+      T* operator* () { return m_items ; }
+      const T* operator* () const { return m_items ; }
+      explicit operator T* () const { return m_items ; }
+      operator const T* () const { return m_items ; }
+      T& operator[] (size_t N) { return m_items[N] ; }
+      const T& operator[] (size_t N) const { return m_items[N] ; }
+      explicit operator bool () const { return m_items != nullptr ; }
+      bool operator ! () const { return m_items == nullptr ; }
    protected:
-      T* m_string ;
+      T* m_items ;
    } ;
 
 } // end of namespace Fr
