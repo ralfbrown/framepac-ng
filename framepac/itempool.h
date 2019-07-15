b3b534f790ef7f0ddc407a33db59b2783d0b4fab
ralfbrown <ralfbrown@users.noreply.github.com>
2019-07-15 13:15:55 -0400

    split ItemPool into ItemPool and ItemPoolFlat (for non-chunked storage)

--- framepac/itempool.h
+++ framepac/itempool.h
@@ -43,12 +43,161 @@ class ItemPoolIter ;			// forward declaration, as we need def of ItemPool
 //   requests to allocate additional items will result in the contents
 //   of the memory-mapped file being copied into an allocated buffer
 
-template <typename T, unsigned chunksize=64>
-class ItemPool
+template <typename T>
+class ItemPoolFlat
    {
    public:
       typedef T* iter_type ;
       typedef const T* const_iter_type ;
+      
+   public:
+      ItemPoolFlat(size_t init_cap = 0)
+	 : m_capacity(0) { if (init_cap) resize(init_cap) ; }
+      ~ItemPoolFlat() { reset() ; }
+
+      size_t alloc()
+	 {
+	    // atomically allocate an index in the array
+	    size_t idx = m_size++ ;
+	    if (idx >= m_capacity)
+	       {
+	       // array was full, so we must resize
+	       // start by grabbing the lock
+	       std::lock_guard<std::mutex> _(s_mutex) ;
+	       // check whether another thread beat us to the resize
+	       if (m_size >= m_capacity)
+		  {
+		  // still need to resize, so do it
+		  // compute the new capacity, then ask to increase to that capacity
+		  size_t cap = m_capacity ;
+		  size_t new_cap = cap < 32 ? 32 : (cap < 65536 ? 2*cap : 3*cap/2) ;
+		  resize(new_cap) ;
+		  }
+	       }
+	    return idx ;
+	 }
+      void release(size_t /*index*/)
+	 {
+	    // we can release the last item allocated; if any more have been allocated since, the
+	    //   request is ignored and that item simply goes to waste
+	    //if (???)
+	       {
+	       // TODO: use CAS to ensure that the compare and decrement happen atomically
+
+	       }
+	 }
+      bool external_buffer(T* base, size_t N)
+	 {
+	    if (!base) return false ;
+	    reset() ;			// remove any existing items
+	    m_items = base ;
+	    m_extdata = m_size = m_capacity = N ;
+	    return true ;
+	 }
+      bool external_buffer(const char* base, size_t N)
+	 { return external_buffer(const_cast<T*>(reinterpret_cast<const T*>(base)),N) ; }
+
+      size_t size() const { return m_size ; }
+      size_t capacity() const { return m_capacity ; }
+      void reserve(size_t new_cap)
+	 {
+	    if (new_cap > m_capacity)
+	       {
+	       std::lock_guard<std::mutex> _(s_mutex) ;
+	       resize(new_cap) ;
+	       }
+	 }
+      void shrink(size_t newsize)  // forget about any elements past 'newsize'
+	 {
+	    if (newsize < m_size) m_size = newsize ;
+	 }
+      size_t allocBatch(size_t incr)
+	 {
+	 size_t currsize = size() ;
+	 size_t newsize = currsize + incr ;
+	 reserve(newsize) ;
+	 m_size = newsize ;
+	 return currsize ;
+	 }
+      void clear() { m_size = 0 ; }
+      
+      bool load(CFile& f, size_t N)
+	 {
+	    if (N == 0) return true ;		// trivially successful
+	    auto start = allocBatch(N) ;
+	    bool success = m_items && f.read(m_items+start,N,sizeof(T)) == N ;
+	    if (success) m_size += N ;
+	    return success ;
+	 }
+      
+      bool save(CFile& f) const
+	 {
+	    if (size() == 0) return true ; 	// trivially successful
+	    return !m_items || f.write(m_items,m_size,sizeof(T)) == m_size ;
+	 }
+      
+      explicit operator bool () const { return capacity() > 0 ; }
+      bool operator ! () const { return capacity() == 0 || !m_items ; }
+
+      // access to the allocated items
+      T& operator[] (size_t N) const
+	 { if (N >= size())
+	       throw std::out_of_range("ItemPoolFlat") ;
+           return m_items[N] ;
+	 }
+      T* item(size_t N) const { return N < size() ? &m_items[N] : nullptr ; }
+
+      // iterator support
+      iter_type begin() const { return m_items ; }
+      const_iter_type cbegin() const { return m_items ; }
+      iter_type end() const { return m_items + m_size ; }
+      const_iter_type cend() const { return m_items + m_size ; }
+
+   protected:
+      void reset()  // set pool back to initial empty state
+	 {
+	    if (m_extdata == 0)
+	       delete[] m_items ;
+	    m_items = nullptr ;
+	    m_size = 0 ;
+	    m_capacity = 0 ;
+	    m_extdata = 0 ;
+	    return ;
+	 }
+      void resize(size_t new_cap)
+	 {
+	    T* new_items = new T[new_cap] ;
+	    if (new_items)
+	       {
+	       size_t cap = m_capacity ;
+	       if (cap && m_items)
+		  std::copy_n(m_items,cap,new_items) ;
+//FIXME	       if (m_ownbuf)
+		  delete[] m_items ;
+	       m_items = new_items ;
+	       m_capacity = new_cap ;
+	       m_extdata = 0 ;
+	       }
+	 }
+   protected:
+      T*          m_items { nullptr } ;	// the buffer for the items (if chunksize==0)
+      atom_size_t m_capacity ;		// size of the allocated array
+      atom_size_t m_size { 0 } ;	// number of items actually in use
+      size_t      m_extdata { 0 } ;	// how many items are stored in an external buffer we don't own?
+      static std::mutex s_mutex ;	// lock for use when resizing m_items
+   } ;
+
+template <typename T>
+std::mutex ItemPoolFlat<T>::s_mutex ;
+
+//----------------------------------------------------------------------
+
+template <typename T, unsigned chunksize=64>
+class ItemPool
+   {
+   public:
+      typedef ItemPoolIter<T,chunksize> iter_type ;
+      typedef const iter_type const_iter_type ;
       class Chunk
 	 {
 	 public:
@@ -95,16 +244,11 @@ class ItemPool
 	 {
 	    if (!base) return false ;
 	    reset() ;			// remove any existing items
-	    if (chunksize == 0)
-	       m_data.m_items = base ;
-	    else
+	    // allocate chunk list and point each at the appropriate segment of the external buffer
+	    m_chunks = new Chunk*[(N/chunksize)+1] ;
+	    for (size_t i = 0 ; i <= N/chunksize ; ++i)
 	       {
-	       // allocate chunk list and point each at the appropriate segment of the external buffer
-	       m_data.m_chunks = new Chunk*[(N/chunksize)+1] ;
-	       for (size_t i = 0 ; i <= N/chunksize ; ++i)
-		  {
-		  m_data.m_chunks[i] = reinterpret_cast<Chunk*>(base) + i ;
-		  }
+	       m_chunks[i] = reinterpret_cast<Chunk*>(base) + i ;
 	       }
 	    m_extdata = m_size = m_capacity = N ;
 	    return true ;
@@ -141,21 +285,14 @@ class ItemPool
 	    if (N == 0) return true ;		// trivially successful
 	    auto start = allocBatch(N) ;
 	    bool success = true;
-	    if (chunksize == 0)
+	    //assert(start == 0); //TODO: handle load into non-empty pool
+	    for (size_t i = 0 ; success && i < N/chunksize ; ++i)
 	       {
-	       success = m_data.m_items && f.read(m_data.m_items+start,N,sizeof(T)) == N ;
+	       success = f.read(m_chunks[i],chunksize,sizeof(T)) == chunksize ;
 	       }
-	    else
-	       {
-	       //assert(start == 0); //TODO: handle load into non-empty pool
-	       for (size_t i = 0 ; success && i < N/chunksize ; ++i)
-		  {
-		  success = f.read(m_data.m_chunks[i],chunksize,sizeof(T)) == chunksize ;
-		  }
-	       unsigned remain = N % chunksize ;
-	       if (success && remain)
-		  success = f.read(m_data.m_chunks[N/chunksize],remain,sizeof(T)) == remain ;
-	       }
+	    unsigned remain = N % chunksize ;
+	    if (success && remain)
+	       success = f.read(m_chunks[N/chunksize],remain,sizeof(T)) == remain ;
 	    if (success) m_size += N ;
 	    return success ;
 	 }
@@ -163,56 +300,46 @@ class ItemPool
       bool save(CFile& f) const
 	 {
 	    if (size() == 0) return true ; 	// trivially successful
-	    if (chunksize == 0)
-	       return !m_data.m_items || f.write(m_data.m_items,m_size,sizeof(T)) == m_size ;
 	    bool success = true ;
 	    for (size_t i = 0 ; success && i < size()/chunksize ; ++i)
 	       {
-	       success = f.write(m_data.m_chunks[i],chunksize,sizeof(T)) == chunksize ;
+	       success = f.write(m_chunks[i],chunksize,sizeof(T)) == chunksize ;
 	       }
 	    unsigned remain = size()%chunksize ;
 	    if (remain && success)
-	       success = f.write(m_data.m_chunks[size()/chunksize],remain,sizeof(T)) == remain ;
+	       success = f.write(m_chunks[size()/chunksize],remain,sizeof(T)) == remain ;
 	    return success ;
 	 }
       
       explicit operator bool () const { return capacity() > 0 ; }
-      bool operator ! () const { return capacity() == 0 || !m_data.m_items ; }
+      bool operator ! () const { return capacity() == 0 || !m_chunks ; }
 
       // access to the allocated items
       T& operator[] (size_t N) const
 	 { if (N >= size())
 	       throw std::out_of_range("ItemPool") ;
-           return (chunksize == 0) ? m_data.m_items[N] : m_data.m_chunks[N/chunksize]->m_items[N%chunksize] ;
+           return m_chunks[N/chunksize]->m_items[N%chunksize] ;
 	 }
       T* item(size_t N) const
-	 { return N < size()
-		    ? ((chunksize == 0) ? &m_data.m_items[N] : &m_data.m_chunks[N/chunksize]->m_items[N%chunksize])
-		    : nullptr ; }
+	 { return N < size() ? &m_chunks[N/chunksize]->m_items[N%chunksize] : nullptr ; }
 
       // iterator support
-      iter_type begin() const { return m_data.m_items ; }
-      const_iter_type cbegin() const { return m_data.m_items ; }
-      iter_type end() const { return m_data.m_items + m_size ; }
-      const_iter_type cend() const { return m_data.m_items + m_size ; }
+      iter_type begin() const { return iter_type(this) ; }
+      const_iter_type cbegin() const { return iter_type(this) ; }
+      iter_type end() const { return iter_type(this,m_size.load()) ; }
+      const_iter_type cend() const { return iter_type(this,m_size.load()) ; }
 
    protected:
       void reset()  // set pool back to initial empty state
 	 {
-	    if (chunksize == 0)
-	       {
-	       if (m_extdata == 0)
-		  delete[] m_data.m_items ;
-	       m_data.m_items = nullptr ;
-	       }
-	    else if (m_size > m_extdata)
+	    if (m_size > m_extdata)
 	       {
 	       for (size_t i = m_extdata/chunksize ; i < (m_size+chunksize-1)/chunksize ; ++i)
 		  {
-		  delete m_data.m_chunks[i] ;
+		  delete m_chunks[i] ;
 		  }
-	       delete[] m_data.m_chunks ;
-	       m_data.m_chunks = nullptr ;
+	       delete[] m_chunks ;
+	       m_chunks = nullptr ;
 	       }
 	    m_size = 0 ;
 	    m_capacity = 0 ;
@@ -221,28 +348,26 @@ class ItemPool
 	 }
       void resize(size_t new_cap)
 	 {
-	    T* new_items = new T[new_cap] ;
-	    if (new_items)
+	    Chunk** new_chunks = new Chunk*[(new_cap/chunksize)+1] ;
+	    if (new_chunks)
 	       {
 	       size_t cap = m_capacity ;
-	       if (cap && m_data.m_items)
-		  std::copy_n(m_data.m_items,cap,new_items) ;
-	       if (m_ownbuf)
-		  delete[] m_data.m_items ;
-	       m_data.m_items = new_items ;
+	       if (cap && m_chunks)
+		  {
+//FIXME:	  std::copy_n(m_data.m_items,cap,new_items) ;
+		  }
+//FIXME	       if (m_ownbuf)
+		  delete[] m_chunks ;
+	       m_chunks = new_chunks ;
 	       m_capacity = new_cap ;
-	       m_ownbuf = true ;
+	       m_extdata = 0 ;
 	       }
 	 }
    protected:
-      union {
-	 Chunk**  m_chunks ;		// the list of chunks (if chunksize > 0)
-	 T*       m_items { nullptr } ;	// the buffer for the items (if chunksize==0)
-	 } m_data ;
+      Chunk**     m_chunks ;		// the list of chunks
       atom_size_t m_capacity ;		// size of the allocated array
       atom_size_t m_size { 0 } ;	// number of items actually in use
       size_t      m_extdata { 0 } ;	// how many items are stored in an external buffer we don't own?
-      bool        m_ownbuf { false } ;	// have we allocated the buffer ourselves?
       static std::mutex s_mutex ;	// lock for use when resizing m_items
    } ;
 
@@ -267,7 +392,12 @@ class ItemPoolIter
 
       T* operator* () const { return m_pool->item(m_index) ; }
       T* operator-> () const { return m_pool->item(m_index) ; }
-      ItemPoolIter& operator++() { m_index++ ; }
+      //operator T* () const { return m_pool->item(m_index) ; }
+      T& operator[] (size_t N) const { return *m_pool->item(m_index) ; }
+      ItemPoolIter& operator++() { ++m_index ; return *this ; }
+      ItemPoolIter& operator--() { --m_index ; return *this ; }
+      ItemPoolIter& operator+=(size_t N) { m_index += N ; return *this ; }
+      ItemPoolIter& operator-=(size_t N) { m_index -= N ; return *this ; }
       bool operator== (const ItemPoolIter& other) const
 	 { return m_index == other.m_index && m_pool == other.m_pool ; }
       bool operator!= (const ItemPoolIter& other) const
@@ -276,6 +406,10 @@ class ItemPoolIter
       const Pool* m_pool ;
       size_t      m_index ;
    } ;
+
+template <typename T, unsigned chunksize>
+std::ptrdiff_t operator- (const ItemPoolIter<T,chunksize>& one, const ItemPoolIter<T,chunksize>& two)
+{ return one.currIndex() - two.currIndex() ; }
 
 } // end namespace Fr
 
