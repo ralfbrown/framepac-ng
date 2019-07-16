89928c5fe05639bd3907de5e290ae6c6cebadc7a
ralfbrown <ralfbrown@users.noreply.github.com>
2019-07-16 01:26:32 -0400

    add .debug() to SystemMessage

--- framepac/message.h
+++ framepac/message.h
@@ -1,10 +1,10 @@
 /****************************** -*- C++ -*- *****************************/
 /*									*/
 /* FramepaC-ng								*/
-/* Version 0.07, last edit 2018-07-16					*/
+/* Version 0.14, last edit 2019-07-16					*/
 /*	by Ralf Brown <ralf@cs.cmu.edu>					*/
 /*									*/
-/* (c) Copyright 2016,2017,2018 Carnegie Mellon University		*/
+/* (c) Copyright 2016,2017,2018,2019 Carnegie Mellon University		*/
 /*	This program may be redistributed and/or modified under the	*/
 /*	terms of the GNU General Public License, version 3, or an	*/
 /*	alternative license agreement as detailed in the accompanying	*/
@@ -50,6 +50,8 @@ class SystemMessage
       _attr_printf_ static bool status(const char* fmt, ...) ;
       static bool status(const char* fmt, std::va_list args) ;
       _attr_printf_ static bool warning(const char* fmt, ...) ;
+      static bool debug(const char* fmt, std::va_list args) ;
+      _attr_printf_ static bool debug(const char* fmt, ...) ;
       static bool warning(const char* fmt, std::va_list args) ;
       _attr_printf_ static bool error(const char* fmt, ...) ;
       static bool error(const char* fmt, std::va_list args) ;
@@ -62,12 +64,13 @@ class SystemMessage
       _attr_printf0_ static bool nomemory(const char* msg) ;
       static bool no_memory(const char* msg) ;
    protected:
-      virtual bool showModal(const char *msg) = 0 ;
-      virtual bool showConfirmation(const char *msg) = 0 ;
-      virtual bool showMessage(const char *msg) = 0 ;
-      virtual bool showWarning(const char *msg) = 0 ;
-      virtual bool showError(const char *msg) = 0 ;
-      virtual bool showFatal(const char *msg) = 0 ;
+      virtual bool showModal(const char* msg) = 0 ;
+      virtual bool showConfirmation(const char* msg) = 0 ;
+      virtual bool showMessage(const char* msg) = 0 ;
+      virtual bool showWarning(const char* msg) = 0 ;
+      virtual bool showDebug(const char* msg) = 0 ;
+      virtual bool showError(const char* msg) = 0 ;
+      virtual bool showFatal(const char* msg) = 0 ;
 
       static SystemMessage* s_instance ;
    } ;
@@ -88,6 +91,7 @@ class ConsoleSystemMessage : public SystemMessage
       virtual bool showConfirmation(const char *msg) ;
       virtual bool showMessage(const char *msg) ;
       virtual bool showWarning(const char *msg) ;
+      virtual bool showDebug(const char* msg) ;
       virtual bool showError(const char *msg) ;
       virtual bool showFatal(const char *msg) ;
    } ;
