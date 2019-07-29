c811bdba94ca52dfb7bcf42af3c841b6b62ba3e9
ralfbrown <ralfbrown@users.noreply.github.com>
2019-07-28 22:03:30 -0400

    add template class VarBits

--- framepac/bits.h
+++ framepac/bits.h
@@ -22,6 +22,7 @@
 #ifndef _Fr_BITS_H_INCLUDED
 #define _Fr_BITS_H_INCLUDED
 
+#include <climits>
 #include <cstdint>
 #include "framepac/init.h"
 
@@ -69,6 +70,74 @@ class BitReverser
       //   for 12 bits, this increases to 16K
       static constexpr unsigned table_bits = 12 ;
    } ;
+
+//----------------------------------------------------------------------------
+
+template <typename T>
+class VarBitsT
+   {
+   public:
+      VarBitsT() = default ;
+      VarBitsT(unsigned len, T val = 0) : m_value(val),m_length(len) {}
+      VarBitsT(const VarBitsT& orig) = default ;
+      ~VarBitsT()= default ;
+      VarBitsT& operator= (const VarBitsT& other)
+	 {
+	    m_value = other.m_value ;
+	    m_length = other.m_length ;
+	    return *this ;
+	 }
+
+      // accessors
+      T value() const { return m_value ; }
+      unsigned length() const { return m_length ; }
+
+      // modifiers
+      void value(T val) { m_value = (val & mask()) ; }
+      void append(bool bit) { append(bit?1:0) ; }
+      void append(unsigned bit)
+	 { if (m_length < max_bits) { m_value = (m_value << 1) | (bit & 1) ; ++m_length ; } }
+      void append(unsigned bits, unsigned num_bits)
+	 {
+	    if (m_length + num_bits < max_bits)
+	       {
+	       m_value = (m_value << num_bits) | (bits & mask(num_bits)) ;
+	       m_length += num_bits ;
+	       }
+	 }
+      void append(const VarBitsT& other) { append(other.m_value,other.m_length) ; }
+      void trim(unsigned keep) { m_value &= mask(keep) ; m_length = (uint8_t)keep ; }
+
+      // operators
+      T operator* () const { return m_value ; }
+      bool operator== (const VarBitsT& other) const
+	 { return m_value == other.m_value && m_length == other.m_length ; }
+      bool operator!= (const VarBitsT& other) const
+	 { return m_value != other.m_value || m_length != other.m_length ; }
+      VarBitsT& operator+= (T add) { m_value = (m_value + add) & mask() ; return *this ; }
+      VarBitsT& operator+= (const VarBitsT& other)
+	 { m_value = (m_value + other.m_value) & mask() ; return *this ; }
+      VarBitsT& operator-= (T sub) { m_value = (m_value - sub) & mask() ; return *this ; }
+      VarBitsT& operator-= (const VarBitsT& other)
+	 { m_value = (m_value - other.m_value) & mask() ; return *this ; }
+
+      // utility functions
+      static T mask(unsigned num_bits) { return (((T)1) << num_bits) - 1 ; }
+      T mask() const { return mask(m_length) ; }
+   private:
+      T       m_value  { 0 } ;
+      uint8_t m_length { 0 } ;
+
+      static constexpr unsigned max_bits = CHAR_BIT * sizeof(T) ;
+   } ;
+
+//----------------------------------------------------------------------
+
+typedef VarBitsT<uint16_t> VarBits16 ;
+typedef VarBitsT<uint32_t> VarBits ;
+typedef VarBitsT<uint64_t> VarBits64 ;
+
+//----------------------------------------------------------------------
 
 } // end namespace Fr
 
